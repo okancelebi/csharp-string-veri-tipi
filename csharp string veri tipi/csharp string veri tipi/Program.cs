21d0474126f737455cc859f7cee3a3db8235d01f
Okan Çelebi <okancelebi06@gmail.com>
2023-01-20 13:10:09 +0300

    Add files via upload

---
+++ csharp string veri tipi/csharp string veri tipi/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_string_veri_tipi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //String veri tipi metinleri tutmak için kullanılır.
+            //Metni çift tırnak içine almalısınız.
+            string İsim = "Okan";
+            string Soyisim = "Çelebi";
+            string c = "Hello World.";
+            Console.WriteLine(İsim);
+            Console.WriteLine(Soyisim);
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
